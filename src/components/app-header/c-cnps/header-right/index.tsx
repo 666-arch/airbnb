67c40d312dd79a37b8a9bb805c655591999bf09c
MaxHuang <hc18174419502@163.com>
2025-07-23 15:37:19 +0800

    header

--- src/components/app-header/c-cnps/header-right/index.tsx
+++ src/components/app-header/c-cnps/header-right/index.tsx
@@ -1,7 +1,25 @@
-import React, { memo } from "react";
+import IconGlobal from "@/assets/svg/icon_global";
+import IconMenu from "@/assets/svg/icon_menu";
+import IconProfile from "@/assets/svg/icon_profile";
+import { memo } from "react";
 
-const HeaderRight = memo(function index(props) {
-  return <div className="flex flex-1 justify-end">HeaderRight</div>;
+const HeaderRight = memo(() => {
+  return (
+    <div className="flex flex-1 justify-end items-center gap-25px">
+      <div className="flex items-center gap-16px font-size-14px font-600">
+        <span className="cursor-pointer hover:bg-#afaeaea1 border-rd-10px p-8px">登录</span>
+        <span className="cursor-pointer hover:bg-#afaeaea1 border-rd-10px p-8px">注册</span>
+        <span className="cursor-pointer">
+          <IconGlobal />
+        </span>
+      </div>
+
+      <div className="flex items-center gap-15px border-rd-22px border-1 border-solid border-#ccc p-5px mr-24px cursor-pointer bg-#FFF">
+        <IconMenu />
+        <IconProfile />
+      </div>
+    </div>
+  );
 });
 
 export default HeaderRight;
