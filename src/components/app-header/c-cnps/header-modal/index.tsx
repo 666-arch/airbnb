bd372b1f42ffd8a27b05aaeeff11cfb030c3e499
MaxHuang <hc18174419502@163.com>
2025-07-24 16:09:56 +0800

    update

--- src/components/app-header/c-cnps/header-modal/index.tsx
+++ src/components/app-header/c-cnps/header-modal/index.tsx
@@ -1,19 +1,24 @@
-import { memo } from "react";
+import { ForwardedRef, forwardRef, memo } from "react";
 
-const HeaderModal = memo(() => {
-  return (
-    <div className="border-1 border-solid border-#000 pos-absolute right-24px top-90px w-160px h-200px flex flex-col justify-around gap-10px bg-#FFF border-rd-10px pl-10px font-size-14px color-#000">
-      <div className="flex flex-col gap-10px border-b-1 border-b-solid border-b-#e6e6e6">
-        <div>注册</div>
-        <div>登录</div>
+const HeaderModal = memo(
+  forwardRef((props, ref: ForwardedRef<HTMLDivElement>) => {
+    return (
+      <div
+        ref={ref}
+        className="border-1 border-solid border-#000 pos-absolute right-24px top-90px w-160px h-200px flex flex-col justify-around gap-10px bg-#FFF border-rd-10px pl-10px font-size-14px color-#000"
+      >
+        <div className="flex flex-col gap-10px border-b-1 border-b-solid border-b-#e6e6e6">
+          <div>注册</div>
+          <div>登录</div>
+        </div>
+        <div className="flex flex-col gap-10px">
+          <div>出租房源</div>
+          <div>开展体验</div>
+          <div>帮助</div>
+        </div>
       </div>
-      <div className="flex flex-col gap-10px">
-        <div>出租房源</div>
-        <div>开展体验</div>
-        <div>帮助</div>
-      </div>
-    </div>
-  );
-});
+    );
+  })
+);
 
 export default HeaderModal;
--- src/components/app-header/c-cnps/header-right/index.tsx
+++ src/components/app-header/c-cnps/header-right/index.tsx
@@ -1,12 +1,20 @@
 import IconGlobal from "@/assets/svg/icon_global";
 import IconMenu from "@/assets/svg/icon_menu";
 import IconProfile from "@/assets/svg/icon_profile";
-import { memo, useState } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import HeaderModal from "../header-modal";
 import { createPortal } from "react-dom";
+import { useClickAway } from "ahooks";
 
 const HeaderRight = memo(() => {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+  const profileRef = useRef<HTMLDivElement>(null);
+
+  useClickAway(() => {
+    setShowModal(false);
+  }, [modalRef, profileRef]);
+
   return (
     <div className="flex flex-1 justify-end items-center gap-25px pos-relative">
       <div className="flex items-center gap-16px font-size-14px font-600">
@@ -21,16 +29,16 @@ const HeaderRight = memo(() => {
         </span>
       </div>
       <div
+        ref={profileRef}
         onClick={() => {
           setShowModal(!showModal);
-          console.log("----------");
         }}
         className="shadow-box flex items-center gap-15px border-rd-22px border-1 border-solid border-#ccc p-5px mr-24px cursor-pointer bg-#FFF"
       >
         <IconMenu />
         <IconProfile />
       </div>
-      {showModal && createPortal(<HeaderModal />, document.body)}
+      {showModal && createPortal(<HeaderModal ref={modalRef} />, document.body)}
     </div>
   );
 });
