fcc7a7963ce2f0a27ba338e35a4fff2674ac2083
MaxHuang <hc18174419502@163.com>
2025-07-24 15:33:14 +0800

    update

--- src/components/app-header/c-cnps/header-right/index.tsx
+++ src/components/app-header/c-cnps/header-right/index.tsx
@@ -1,23 +1,36 @@
 import IconGlobal from "@/assets/svg/icon_global";
 import IconMenu from "@/assets/svg/icon_menu";
 import IconProfile from "@/assets/svg/icon_profile";
-import { memo } from "react";
+import { memo, useState } from "react";
+import HeaderModal from "../header-modal";
+import { createPortal } from "react-dom";
 
 const HeaderRight = memo(() => {
+  const [showModal, setShowModal] = useState<boolean>(false);
   return (
-    <div className="flex flex-1 justify-end items-center gap-25px">
+    <div className="flex flex-1 justify-end items-center gap-25px pos-relative">
       <div className="flex items-center gap-16px font-size-14px font-600">
-        <span className="cursor-pointer hover:bg-#afaeaea1 border-rd-10px p-8px">登录</span>
-        <span className="cursor-pointer hover:bg-#afaeaea1 border-rd-10px p-8px">注册</span>
+        <span className="cursor-pointer hover:bg-#afaeaea1 border-rd-10px p-8px">
+          登录
+        </span>
+        <span className="cursor-pointer hover:bg-#afaeaea1 border-rd-10px p-8px">
+          注册
+        </span>
         <span className="cursor-pointer">
           <IconGlobal />
         </span>
       </div>
-
-      <div className="shadow-box flex items-center gap-15px border-rd-22px border-1 border-solid border-#ccc p-5px mr-24px cursor-pointer bg-#FFF">
+      <div
+        onClick={() => {
+          setShowModal(!showModal);
+          console.log("----------");
+        }}
+        className="shadow-box flex items-center gap-15px border-rd-22px border-1 border-solid border-#ccc p-5px mr-24px cursor-pointer bg-#FFF"
+      >
         <IconMenu />
         <IconProfile />
       </div>
+      {showModal && createPortal(<HeaderModal />, document.body)}
     </div>
   );
 });
