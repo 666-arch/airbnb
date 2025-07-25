35e638819c215061d946bf34fc34dbd28b791c99
MaxHuang <hc18174419502@163.com>
2025-07-25 11:08:31 +0800

    add image

---
+++ src/views/Home/c-cnps/home-banner/index.tsx
@@ -0,0 +1,7 @@
+import React, { memo } from "react";
+import './index.less'
+const HomeBanner = memo(() => {
+  return <div className="home-banner-wrapper h-529px"></div>;
+});
+
+export default HomeBanner;
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,18 +1,21 @@
-import apiRequest from "@/services";
+// import apiRequest from "@/services";
 import React, { memo, useEffect } from "react";
+import HomeBanner from "./c-cnps/home-banner";
 
 const Home = memo(() => {
-  useEffect(() => {
-    apiRequest
-      .get({
-        url: "/home/highscore",
-      })
-      .then((res) => {
-        console.log(res);
-      });
-  }, []);
-  return <div>
-    <h1>Home Page</h1>
-  </div>;
+  // useEffect(() => {
+  //   apiRequest
+  //     .get({
+  //       url: "/home/highscore",
+  //     })
+  //     .then((res) => {
+  //       console.log(res);
+  //     });
+  // }, []);
+  return (
+    <div className="home-page-wrapper">
+      <HomeBanner/>
+    </div>
+  );
 });
 export default Home;
