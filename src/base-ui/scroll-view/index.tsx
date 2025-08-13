0b439336cf9504001e311f280d4ef7baa6c9b33b
MaxHuang <hc18174419502@163.com>
2025-08-13 16:33:19 +0800

    update

--- src/base-ui/scroll-view/index.tsx
+++ src/base-ui/scroll-view/index.tsx
@@ -4,32 +4,37 @@ interface IProps {
 }
 const ScrollView: React.FC<IProps> = memo((props) => {
   const [showRightBtn, setShowRightBtn] = useState<boolean>(false);
-  const slotRef = useRef<HTMLDivElement>(null);
+  const [showLeftBtn, setShowLeftBtn] = useState<boolean>(false);
   const [tabIndex, setTabIndex] = useState<number>(0);
+  const tabIndexRef = useRef<number>(0)
   const totalDistanceRef = useRef<number>(0);
-
+  const slotRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (slotRef.current) {
       const ctxClientWidth = slotRef.current.clientWidth; //内容可视长度
       const ctxScrollWidth = slotRef.current.scrollWidth; //滚动条长度
       const totalDistance = ctxScrollWidth - ctxClientWidth; //可滚动距离总长度
-      totalDistanceRef.current = totalDistance
+      totalDistanceRef.current = totalDistance;
       setShowRightBtn(totalDistance > 0);
     }
   }, [props.children]);
-  const handleRightClick = () => {
-    const newTabIndex = tabIndex + 1;
-    setTabIndex(newTabIndex);
+  const handleControlClick = (isRight: boolean) => {
+    // const newTabIndex = tabIndex + 1;
+    const newTabIndex = isRight ? tabIndex + 1 : tabIndex -1;
     const childElement = slotRef.current.children[newTabIndex];
-    // console.log('---', childElement.offsetLeft)
     slotRef.current.style.transform = `translate(-${childElement.offsetLeft}px)`;
-    setShowRightBtn(totalDistanceRef.current > childElement.offsetLeft)
+    setShowRightBtn(totalDistanceRef.current > childElement.offsetLeft);
+    setShowLeftBtn(childElement.offsetLeft > 0);
+    setTabIndex(newTabIndex);
   };
-  const handleLeftClick = () => {};
   return (
     <div className="scroll-view-info pos-relative overflow-hidden">
-      <button onClick={handleLeftClick}>左边按钮</button>
-      {showRightBtn && <button onClick={handleRightClick}>右边按钮</button>}
+      {showLeftBtn && (
+        <button onClick={() => handleControlClick(false)}>左边按钮</button>
+      )}
+      {showRightBtn && (
+        <button onClick={() => handleControlClick(true)}>右边按钮</button>
+      )}
       <div className="slot flex gap-18px" ref={slotRef}>
         {props.children}
       </div>
