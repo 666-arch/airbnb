95d8139c8cb4518e5af171c913a54f405fcc71e1
MaxHuang <hc18174419502@163.com>
2025-08-13 17:16:06 +0800

    update

--- src/base-ui/scroll-view/index.tsx
+++ src/base-ui/scroll-view/index.tsx
@@ -1,3 +1,5 @@
+import IconArrowLeft from "@/assets/svg/icon_arrow_left";
+import IconArrowRight from "@/assets/svg/icon_arrow_right";
 import React, { memo, useEffect, useRef, useState } from "react";
 interface IProps {
   children?: React.ReactNode;
@@ -6,7 +8,7 @@ const ScrollView: React.FC<IProps> = memo((props) => {
   const [showRightBtn, setShowRightBtn] = useState<boolean>(false);
   const [showLeftBtn, setShowLeftBtn] = useState<boolean>(false);
   const [tabIndex, setTabIndex] = useState<number>(0);
-  const tabIndexRef = useRef<number>(0)
+  const tabIndexRef = useRef<number>(0);
   const totalDistanceRef = useRef<number>(0);
   const slotRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
@@ -19,25 +21,38 @@ const ScrollView: React.FC<IProps> = memo((props) => {
     }
   }, [props.children]);
   const handleControlClick = (isRight: boolean) => {
-    // const newTabIndex = tabIndex + 1;
-    const newTabIndex = isRight ? tabIndex + 1 : tabIndex -1;
-    const childElement = slotRef.current.children[newTabIndex];
-    slotRef.current.style.transform = `translate(-${childElement.offsetLeft}px)`;
-    setShowRightBtn(totalDistanceRef.current > childElement.offsetLeft);
-    setShowLeftBtn(childElement.offsetLeft > 0);
-    setTabIndex(newTabIndex);
+    if (slotRef.current) {
+      const newTabIndex = isRight ? tabIndex + 1 : tabIndex - 1;
+      const childElement = slotRef.current.children[newTabIndex] as HTMLElement;
+      slotRef.current.style.transform = `translate(-${childElement?.offsetLeft}px)`;
+      setShowRightBtn(totalDistanceRef.current > childElement?.offsetLeft);
+      setShowLeftBtn(childElement?.offsetLeft > 0);
+      setTabIndex(newTabIndex);
+    }
   };
   return (
-    <div className="scroll-view-info pos-relative overflow-hidden">
+    <div className="scroll-view-info flex pos-relative items-center">
       {showLeftBtn && (
-        <button onClick={() => handleControlClick(false)}>左边按钮</button>
+        <div
+          className="cursor-pointer pos-absolute left--20px"
+          onClick={() => handleControlClick(false)}
+        >
+          <IconArrowLeft />
+        </div>
       )}
-      {showRightBtn && (
-        <button onClick={() => handleControlClick(true)}>右边按钮</button>
-      )}
-      <div className="slot flex gap-18px" ref={slotRef}>
-        {props.children}
+      <div className="overflow-hidden">
+        <div className="slot flex gap-19px" ref={slotRef}>
+          {props.children}
+        </div>
       </div>
+      {showRightBtn && (
+        <div
+          className="cursor-pointer pos-absolute right--10px"
+          onClick={() => handleControlClick(true)}
+        >
+          <IconArrowRight />
+        </div>
+      )}
     </div>
   );
 });
