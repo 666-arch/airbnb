efaafdef3f8fa795ecece7279911c96e1766f1bc
MaxHuang <hc18174419502@163.com>
2025-08-14 10:33:00 +0800

    update style

--- src/components/longfor-item/index.tsx
+++ src/components/longfor-item/index.tsx
@@ -6,8 +6,11 @@ const LongForItem: React.FC<IProps> = memo(({ itemData }) => {
   return (
     <div className="longfor-item-info pos-relative">
       <img src={itemData.picture_url} className="w-200px" />
-      <div className="cover"></div>
-      <div className="w-200px pos-absolute bottom-25px flex flex-col items-center gap-5px">
+      <div
+        className="w-200px h-246px pos-absolute top-0px"
+        style={{ backgroundColor: "#000", zIndex: 1, opacity: 0.2 }}
+      ></div>
+      <div className="w-200px pos-absolute bottom-25px flex flex-col items-center gap-5px z-2">
         <div>{itemData.city}</div>
         <div>均价 {itemData.price}</div>
       </div>
