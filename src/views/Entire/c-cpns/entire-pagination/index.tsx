4fce6c761c7b2b2ddd560ceaf11a54bf8f6d19c7
MaxHuang <hc18174419502@163.com>
2025-08-14 16:49:12 +0800

    update

---
+++ src/views/Entire/c-cpns/entire-pagination/index.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react'
+
+const EntirePagintion = memo(({}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+})
+
+export default EntirePagintion
---
+++ src/views/Entire/c-cpns/entire-rooms/index.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react'
+
+const EntireRooms = memo(({}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+})
+
+export default EntireRooms
