d9ba8faf5a89b7c9f44f230306b6f3eb70f52399
MaxHuang <hc18174419502@163.com>
2025-08-15 16:15:12 +0800

    update

--- src/views/Entire/c-cpns/entire-rooms/index.tsx
+++ src/views/Entire/c-cpns/entire-rooms/index.tsx
@@ -3,25 +3,30 @@ import React, { memo } from "react";
 import { useSelector } from "react-redux";
 
 const EntireRooms = memo(({}) => {
-  const { roomList } = useSelector(
-    (state: { entire: { roomList: RoomListType[] } }) => ({
+  const { roomList, totalCount } = useSelector(
+    (state: { entire: { roomList: RoomListType[], totalCount: number } }) => ({
       roomList: state.entire.roomList,
+      totalCount: state.entire.totalCount
     })
   );
   return (
-    <div className="entire-rooms-info flex flex-wrap px-20px">
-      {roomList.length > 0 &&
-        roomList.map((item) => (
-          <RoomItem
-            key={item.id}
-            name={item.name}
-            verify_info={item.verify_info}
-            price_format={item.price_format}
-            star_rating={item.reviews_count}
-            picture_url={item.picture_url}
-            width="20%"
-          />
-        ))}
+    <div className="entire-rooms-info flex flex-col flex-wrap">
+      <div className="px-28px font-700 mt-10px">共{totalCount}多处住所</div>
+      {roomList.length > 0 && (
+        <div className="flex flex-wrap px-20px py-20px font-size-22px">
+          {roomList.map((item) => (
+            <RoomItem
+              key={item.id}
+              name={item.name}
+              verify_info={item.verify_info}
+              price_format={item.price_format}
+              star_rating={item.reviews_count}
+              picture_url={item.picture_url}
+              width="20%"
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 });
