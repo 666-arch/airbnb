7a8ae20a742fe2bdcc73eeee07de7f0c336c254c
MaxHuang <hc18174419502@163.com>
2025-09-04 10:48:06 +0800

    readme update

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import { Provider } from "react-redux";
 import store from "@/store";
 import "virtual:uno.css";
 import "./index.less";
-import { HashRouter } from "react-router-dom";
-import { Suspense } from "react";
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <App />
